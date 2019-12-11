e62144775482b857c500eed09b4ff0bfd0861a80
LucasVidigal98 <lucasvidigal3@gmail.com>
2019-12-11 03:58:43 -0300

    Seting 4 players

--- src/serverpackage/ServerGame.java
+++ src/serverpackage/ServerGame.java
@@ -22,6 +22,181 @@ public class ServerGame {
     public static int numberConnections = 0;
     public static int portBase = 10000;
     public Integer[][] pos = new Integer[4][4];
+    public static int[] count = new int[4];
+    public static int lose;
+    
+    public class ThreadWin extends Thread{
+        
+        public int id;
+        
+        public ThreadWin(int id){
+            this.id = id;
+        }
+        
+        @Override
+        public void run(){
+            
+            lose = 0;
+            count[0] = 0;
+            count[1] = 0;
+            count[2] = 0;
+            count[3] = 0;
+            
+            boolean win = false, lost = false;
+            int port = 0;
+            int winner = 0;
+            
+            if (id == 0) port = ServerGame.portBase+400;
+            else if (id == 1) port = ServerGame.portBase+401;
+            else if (id == 2) port = ServerGame.portBase+402;
+            else if (id == 3) port = ServerGame.portBase+403;
+            
+            ServerSocket server = null;
+            
+            try {
+                server = new ServerSocket(port);
+            } catch (IOException ex) {
+                Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            System.out.println(" & Servidor conectado na porta " + port);
+            
+            while(true){
+                
+                Socket client = null;
+                ObjectOutputStream io = null;
+                
+                try {
+                    client = server.accept();
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                if(count[0] >= 3){
+                    win = true;
+                    winner = 0;
+                }else if(count[1] >= 3){
+                    win = true;
+                    winner = 1;
+                }else if(count[2] >= 3){
+                    win = true;
+                    winner = 1;
+                }else if(count[3] >= 3){
+                    win = true;
+                    winner = 3;
+                }
+                
+                if(lose >= 4) lost = true;
+                
+                try {
+                    io = new ObjectOutputStream(client.getOutputStream());
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                try {
+                    io.flush();
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                if(win){
+                    
+                    String w = "";
+                    
+                    if(winner == 0){
+                        w = "A galinha barnca Venceu!";
+                        try {
+                            io.writeObject(w);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }else if(winner == 1){
+                        w = "A galinha azul Venceu!";
+                        try {
+                            io.writeObject(w);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }else if(winner == 2){
+                        w = "A galinha laranja Venceu!";
+                        try {
+                            io.writeObject(w);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }else if(winner == 3){
+                        w = "A galinha preta Venceu";
+                        try {
+                            io.writeObject(w);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                        try {
+                            io.close();
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                        pos[0][1] = 44;
+                        pos[0][2] = 600;
+                        pos[1][1] = 364;
+                        pos[1][2] = 600;
+                        pos[2][1] = 626;
+                        pos[2][2] = 600;
+                        pos[3][1] = 940;
+                        pos[3][2] = 600; 
+                        
+                        //break;
+                    }
+                    
+                }else{
+                    
+                    String w = "NULL";
+                    
+                    try {
+                        io.writeObject(w);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                if(lost){
+                    String l = "Todas as galinhas morreram!!";
+                    System.out.println("Vai mandar " + l);
+                    try {
+                        io.writeObject(l);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    pos[0][1] = 44;
+                    pos[0][2] = 600;
+                    pos[1][1] = 364;
+                    pos[1][2] = 600;
+                    pos[2][1] = 626;
+                    pos[2][2] = 600;
+                    pos[3][1] = 940;
+                    pos[3][2] = 600;
+                    
+                    //break;
+                }
+                
+                try {
+                    io.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                try {
+                    client.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }  
+        }
+    }
    
     public class ThreadListen extends Thread{
         
@@ -73,6 +248,14 @@ public class ServerGame {
                     client.close();
                 } catch (IOException ex) {
                     Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                if(pos[this.id][2] == -16){
+                    count[this.id]++;
+                }
+                
+                if(pos[this.id][2] == -200){
+                    lose++;
                 }
                 
                 System.out.println(pos[this.id][0]);
@@ -130,7 +313,7 @@ public class ServerGame {
                     Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
-                if(numberConnections >= 3){
+                if(numberConnections >= 4){
                     play = true;
                     try {
                        io.writeObject(play);
@@ -165,7 +348,9 @@ public class ServerGame {
             ThreadListen t = new ThreadListen();
             t.id = this.id;
             t.start();
-          
+            
+            new ThreadWin(this.id).start();
+            
             try {
                server = new ServerSocket(port+200);
             } catch (IOException ex) {
@@ -223,16 +408,13 @@ public class ServerGame {
     public class ThreadConnection extends Thread{
         
         public int id;
-        
+     
         @Override
         public void run(){
             
+            Integer[] vec = new Integer[2];
             ServerSocket server = null;
-            int port = 0;
-            if (id == 0) port = ServerGame.portBase;
-            else if (id == 1) port = ServerGame.portBase+1;
-            else if (id == 2) port = ServerGame.portBase+2;
-            else if (id == 3) port = ServerGame.portBase+3;
+            int port = ServerGame.portBase;
             
             try {
                server = new ServerSocket(port);
@@ -242,7 +424,7 @@ public class ServerGame {
             
             System.out.println("Servidor conectado na porta " + port);
             
-            //while(true){
+            while(true){
                 Socket client = null;
                 
                 try {
@@ -250,6 +432,15 @@ public class ServerGame {
                 } catch (IOException ex) {
                     Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
                 }
+                
+                if(numberConnections == 4) {
+                    numberConnections = 0;
+                    ServerGame.portBase += 1000;
+                }
+                
+                vec[0] = numberConnections;
+                vec[1] = ServerGame.portBase;
+                
                 
                 //System.out.println("Cliente conectado " + client.getInetAddress() + " Porta " + port);
                 ObjectOutputStream io = null;
@@ -267,7 +458,7 @@ public class ServerGame {
                 }
                 
                 try {
-                   io.writeObject(numberConnections);
+                   io.writeObject(vec);
                    numberConnections++;
                 } catch (IOException ex) {
                     Logger.getLogger(ServerGame.class.getName()).log(Level.SEVERE, null, ex);
@@ -286,10 +477,9 @@ public class ServerGame {
                 }
                 
                 ThreadSend t = new ThreadSend();
-                t.id = this.id;
+                t.id = numberConnections-1;
                 t.start();
-               // break;
-            //}
+            }
         }
     }
     
@@ -298,18 +488,6 @@ public class ServerGame {
         ThreadConnection t1 = new ThreadConnection();
         t1.id = 0;
         t1.start();
-        
-        ThreadConnection t2 = new ThreadConnection();
-        t2.id = 1;
-        t2.start();
-        
-        ThreadConnection t3 = new ThreadConnection();
-        t3.id = 2;
-        t3.start();
-        
-        ThreadConnection t4 = new ThreadConnection();
-        t4.id = 3;
-        t4.start();
     }
     
     public static void main(String[] args) {
